97ec707908e5ba601c9cb8ba473542c02eaced75
Nolan Whittaker <152032773+NolanWhittaker1@users.noreply.github.com>
2024-05-22 18:32:51 -0700

    Added Transitions
    -

--- src/components/Mainpage.tsx
+++ src/components/Mainpage.tsx
@@ -3,6 +3,7 @@ import "./Mainpage.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import tim from "./timothy.png";
 
 function Mainpage() {
   useEffect(() => {
@@ -11,14 +12,22 @@ function Mainpage() {
   return (
     <>
       <Navbar />
-      <section data-aos="fade-left" style={{ backgroundColor: "blue" }}>
-        Timothy Notland
-      </section>
-      <section
-        data-aos="fade-left"
-        style={{ backgroundColor: "black", color: "white" }}
-      >
-        Massimo Notland
+      <section style={{ backgroundColor: "white" }}>
+        <span data-aos="fade-right" data-aos-duration="1500" className="shadow">
+          <h1>Buy our batteries :3</h1>
+          <p>
+            I love downhole battery. I love downhole Battery I love downhole
+            battery. I love downhole Battery I love downhole battery. I love
+            downhole Battery I love downhole battery. I love downhole Battery
+          </p>
+        </span>
+        <img
+          src="/timothy.png"
+          alt="Logo"
+          data-aos="fade-left"
+          data-aos-duration="1500"
+          className="shadow"
+        />
       </section>
     </>
   );
